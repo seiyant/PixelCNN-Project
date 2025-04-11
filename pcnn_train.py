e25deb1db6635aa071afeac0f64d41bea518de46
seiyant <seiyant@student.ubc.ca>
2025-04-11 16:16:17 -0700

    conditional edit

--- pcnn_train.py
+++ pcnn_train.py
@@ -22,21 +22,54 @@ def train_or_test(model, data_loader, optimizer, loss_op, device, args, epoch, m
         
     deno =  args.batch_size * np.prod(args.obs) * np.log(2.)        
     loss_tracker = mean_tracker()
+
+    if mode != 'training':
+        num_correct = 0
+        num_total = 0
     
     for batch_idx, item in enumerate(tqdm(data_loader)):
-        model_input, _ = item
+        model_input, categories = item #add categories
         model_input = model_input.to(device)
-        model_output = model(model_input)
+
+        label_int = [my_bidict[category] for category in categories]
+        label_tensor = torch.tensor(label_int, dtype=torch.long).to(device) #convert string to int label
+
+        model_output = model(model_input, class_label=label_tensor) #pass label tensor
         loss = loss_op(model_input, model_output)
         loss_tracker.update(loss.item()/deno)
+
+        if mode != 'training': #in evaluation, compute predicted labels and update accuracy
+            predicted_labels = get_label(model, model_input)
+            num_correct += (predicted_labels == label_tensor).sum().item()
+            num_total += model_input.size(0)
+
         if mode == 'training':
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
         
+    if mode != 'training':
+        accuracy = num_correct / num_total
+        print(f"{mode.capitalize()} Accuracy: {accuracy:.2%}")
+        if args.en_wandb:
+            wandb.log({mode + "-Accuracy": accuracy})
+
     if args.en_wandb:
         wandb.log({mode + "-Average-BPD" : loss_tracker.get_mean()})
         wandb.log({mode + "-epoch": epoch})
+
+def get_label(model, model_input):
+    model.eval()
+    batch_size = model_input.size(0)
+    class_losses = []
+    for i in range(len(my_bidict)):
+        label_tensor = torch.full((batch_size,), i, dtype=torch.long).to(device) #create tensor with candidate for batch
+        out = model(model_input, class_label=label_tensor) #forward pass with candidate
+        loss_per_sample = discretized_mix_logistic_loss(model_input, out, reduce=False) #loss per sample
+        class_losses.append(loss_per_sample)
+    class_losses = torch.stack(class_losses) #size (NUM_CLASSES, batch_size)
+    predicted_labels = torch.argmin(class_losses, dim=0) #choose the class with smallest loss as prediction per sample
+    return predicted_labels
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
