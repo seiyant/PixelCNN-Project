e2f12a48aab0eaa32e07f07ff1c0ed6378767c71
seiyant <seiyant@student.ubc.ca>
2025-04-12 17:09:31 -0700

    checkpoint save point

--- pcnn_train.py
+++ pcnn_train.py
@@ -228,10 +228,6 @@ if __name__ == '__main__':
         if (epoch + 1) % args.save_interval == 0: 
             if not os.path.exists("models"):
                 os.makedirs("models")
-            checkpoint = {
-                'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-            }
-            torch.save(model.state_dict(), 'models/conditional_pixelcnn.pth')
-            torch.save(model.state_dict(), 'models/{}_{}.pth'.format(model_name, epoch))
-            wandb.save('models/{}_{}.pth'.format(model_name, epoch))
+            checkpoint = 'models/{}_{}.pth'.format(model_name, epoch)
+            torch.save(model.state_dict(), checkpoint)
+            wandb.save(checkpoint)
